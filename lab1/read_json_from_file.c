8504b39b78674aa375061f7ef35e8d6dcfcb7a22
Dmitrii Nashtykov <dmitrii-nashtykov@ya.ru>
2022-03-02 20:10:13 +0300

    dev-5.3 / Функция чтения JSON из файла 	- Добавил основные файлы функции

---
+++ lab1/read_json_from_file.c
@@ -0,0 +1,12 @@
+//
+// Created by dimitis on 02.03.2022.
+//
+#include <unistd.h>
+#include <string.h>
+
+#include "read_json_from_file.h"
+
+int readJSONFromFile(int fileId, char* buffer)
+{
+
+}
---
+++ lab1/read_json_from_file.h
@@ -0,0 +1,10 @@
+//
+// Created by dimitis on 02.03.2022.
+//
+
+#ifndef OS_READ_JSON_FROM_FILE_H
+#define OS_READ_JSON_FROM_FILE_H
+
+int readJSONFromFile(int fileId, char* buffer);
+
+#endif //OS_READ_JSON_FROM_FILE_H
