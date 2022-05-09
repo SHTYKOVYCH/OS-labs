edce9f320e0c66c82c49dfda1b694b02bbbadb0a
Dmitrii <ya.Dimonus.net@ya.ru>
2022-05-09 21:07:00 +0300

    labs 4 / Трубы     - Сделал перенаправление

---
+++ lab4/encoder/encoder.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    FILE* tty = fopen("/dev/tty", "w+");
+
+    size_t len = 0;
+    char string[4096] = {0};
+    char key;
+
+    scanf("%c\n", &key);
+
+    char tmp;
+
+    tmp = getchar();
+    while (tmp != '\n' && tmp != EOF && tmp != 0) {
+        string[len] = tmp;
+        len += 1;
+        tmp = getchar();
+    }
+
+    printf("%c\n", key);
+
+    for (int i = 0; i < len; ++i) {
+        printf("%c", string[i] ^ key);
+    }
+
+    fclose(tty);
+
+    exit(EXIT_SUCCESS);
+}
